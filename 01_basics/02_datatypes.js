2d2756352462d3ea71beea2691da45b3eaadf7f1
Harshavardhan <131587892+Happyhappycat@users.noreply.github.com>
2025-01-16 14:29:28 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict" // treat all JS code as newer version
+// alert(3+3) // we are using node.js not browser
+
+//datatypes- number,string,boolean, Null, undefined, symbol
+
+let dtype=typeof("Harshavardhan")
+console.log(dtype)
