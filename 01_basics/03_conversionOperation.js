9bf41c3b4e6e1db3417c39c4c6e55d85266b9d8a
Harshavardhan <131587892+Happyhappycat@users.noreply.github.com>
2025-01-17 06:32:24 +0000

    functions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,4 +6,20 @@ let flag=true
 
 let numflag=Number(flag)
 
-console.table([numscore,numflag])
+//console.table([numscore,numflag])//
+
+
+function ispresent(i,nums){
+    for(i=0;i<nums.length;++i){
+        if(nums[i]==1){
+            return true
+            
+        }
+        
+        
+    }
+    return false
+}
+
+arr=[0,2,4,2,1]
+console.log(ispresent(0,arr))
