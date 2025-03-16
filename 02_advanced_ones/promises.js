30f300be6907ea2a7e736c0a2033682ecbcc40bc
Harshavardhan <131587892+Happyhappycat@users.noreply.github.com>
2025-03-16 10:32:04 +0000

    Final

--- 02_advanced_ones/promises.js
+++ 02_advanced_ones/promises.js
@@ -1,1 +1,13 @@
-const
+const promiseOne = new Promise(function(resolve,reject){
+    // Do Async Task 
+    // DB Calls, Cryptography, network 
+    setTimeout(function(){
+        console.log("Async Task is Complete");
+        resolve()
+
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise Consumed");
+})
